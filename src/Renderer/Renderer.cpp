f8c05ae6f3347fe82e2246b5f4f5722f4fa33ac3
Tsvetelin Dimitrov <tsvetelin.dim@gmail.com>
2025-03-13 14:40:39 +0100

    Add DearImGui library

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -19,10 +19,10 @@ namespace Athena
         _vertexArray->Bind();
 
         float vertices[] = {
-             0.5f,  0.5f, 0.0f,   0.0f, 0.5f, 1.0f, 1.0f, // top right
-             0.5f, -0.5f, 0.0f,   0.0f, 0.5f, 1.0f, 1.0f, // bottom right
-            -0.5f, -0.5f, 0.0f,   0.0f, 0.5f, 1.0f, 1.0f, // bottom left
-            -0.5f,  0.5f, 0.0f,   0.0f, 0.5f, 1.0f, 1.0f, // top left 
+             0.5f,  0.5f, 0.0f,   0.0f, 0.0f, 0.0f, 1.0f, // top right
+             0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f, 1.0f, // bottom right
+            -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f, 1.0f, // bottom left
+            -0.5f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f, 1.0f, // top left 
         };
         uint32_t vertexBytes = sizeof(vertices);
         _vertexBuffer = Ref<VertexBuffer>::Create(vertices, vertexBytes);
